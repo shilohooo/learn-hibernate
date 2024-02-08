c62f2ba311a0814f2868d031747ebfd6d7a48e47
shiloh <lixiaolei595@gmail.com>
2024-02-08 12:56:32 +0800

    docs: add comment

--- quick-start/src/main/java/org/shiloh/entity/Event.java
+++ quick-start/src/main/java/org/shiloh/entity/Event.java
@@ -10,6 +10,10 @@ import java.time.LocalDateTime;
 
 /**
  * 事件实体
+ * 每个实体都应该提供无参构造器，Hibernate 需要利用反射访问该构造器，以实例化该对象。
+ * <p>
+ * 无参构造器的访问级别应为：public、package-private、private，以便 Hibernate 能够为该实体
+ * 创建代理，以及优化字段访问的代码（getter、setter），例如：在 getter 中实现懒加载
  *
  * @author shiloh
  * @date 2024/2/7 16:55
@@ -22,12 +26,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Event {
     /**
+     * 每个实体都必须有一个唯一标识符
+     * <p>
      * 自增主键
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 事件标题
+     */
     private String title;
 
     /**
